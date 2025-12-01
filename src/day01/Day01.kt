b2bd27e07add2f8906720392c1744eb65c027430
Oskar Giljegard <oskar@giljegard.com>
2025-12-01 06:24:35 +0100

    refactored day 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -4,14 +4,13 @@ import java.io.File
 
 fun main() {
     val lines = File("src/day01/input.txt").readLines()
-    println(lines)
+
+    // Part 1
     var zeroTimes = 0
     var curr = 50
-    /*
     for (line in lines) {
         val firstLetter = line.first()
         val rest = line.substring(1).toInt()
-        /*
         when (firstLetter) {
             'L' -> curr = (curr - rest) % 100
             'R' -> curr = (curr + rest) % 100
@@ -20,30 +19,12 @@ fun main() {
         if (curr == 0) {
             zeroTimes++
         }
-
-        // Part 1: 19, 20, 1147
-         */
-        /*
-        when (firstLetter) {
-            'L' -> curr = (curr - rest)
-            'R' -> curr = (curr + rest)
-            else -> throw IllegalArgumentException("Unknown letter '$firstLetter'")
-        }
-        if (curr == 0) {
-            zeroTimes++
-        }
-        while (curr < 0) {
-            curr += 100
-            zeroTimes++
-        }
-        while (curr >= 100) {
-            curr -= 100
-            zeroTimes++
-        }
-         */
     }
-     */
+    println(zeroTimes)
 
+    // Part 2
+    zeroTimes = 0
+    curr = 50
     val linesExpanded = lines.flatMap { line ->
         val firstLetter = line.first()
         val rest = line.substring(1).toInt()
@@ -60,7 +41,6 @@ fun main() {
         if (curr == 0) {
             zeroTimes++
         }
-        // Part 2: 6796, 7273, 6701 (off by 1 in expanded loop), 6789
     }
     println(zeroTimes)
 }
