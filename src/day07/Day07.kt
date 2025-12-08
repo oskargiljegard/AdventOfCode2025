239ee1cb0aecf91b2db123c761346a86bfc5112d
Oskar Giljegard <oskar@giljegard.com>
2025-12-08 05:58:24 +0100

    refactored day 7

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -4,70 +4,39 @@ import utils.Vector
 import utils.asCharGrid
 import java.io.File
 
+data class Point(var mul: Long, val isSplit: Boolean) {
+    override fun toString(): String {
+        return "$mul "
+    }
+}
 
 fun main() {
-    val grid = File("src/day07/input.txt").readText().asCharGrid { it.toString() }
-    println(grid)
-    val start = grid.positions.filter { grid[it] == "S" }
-    println(start)
-    /*
+    val grid = File("src/day07/input.txt").readText().asCharGrid {
+        return@asCharGrid when (it) {
+            'S' -> Point(1L, false)
+            '.' -> Point(0L, false)
+            '^' -> Point(0L, true)
+            else -> error("Invalid input")
+        }
+    }
+
     var splits = 0
-    for (yPos in grid.yPositions) {
-        if (yPos == 0) continue
+    for (yPos in grid.yPositions.drop(1)) {
         for (xPos in grid.xPositions) {
             val pos = Vector(xPos, yPos)
-            val curr = grid[pos]
             val above = grid[pos + Vector(0, -1)]
-            if (above == 'S' || above == '|') {
-                if (curr == '.') {
-                    grid[pos] = '|'
-                }
-                if (curr == '^') {
+            if (above.mul > 0) {
+                if (grid[pos].isSplit) {
                     splits++
-                    grid[pos + Vector(-1, 0)] = '|'
-                    grid[pos + Vector(1, 0)] = '|'
+                    grid[pos + Vector(-1, 0)].mul += above.mul
+                    grid[pos + Vector(1, 0)].mul += above.mul
+                } else {
+                    grid[pos].mul += above.mul
                 }
+
             }
         }
     }
-    println(grid)
     println(splits)
-     */
-    var splits = 0
-    for (yPos in grid.yPositions) {
-        if (yPos == 0) continue
-        for (xPos in grid.xPositions) {
-            val pos = Vector(xPos, yPos)
-            val curr = grid[pos]
-            val above = grid[pos + Vector(0, -1)]
-            if (above == "S" || above == "|" || above.contains(Regex("\\d"))) {
-                val aboveVal = if (above == "|" || above == "S") 1 else above.toLong()
-                if (curr == ".") {
-                    grid[pos] = aboveVal.toString()
-                }
-                if (curr == "^") {
-                    splits++
-                    val leftPos = pos + Vector(-1, 0)
-                    if (grid[leftPos] == ".") {
-                        grid[leftPos] = aboveVal.toString()
-                    } else if (grid[leftPos].contains(Regex("\\d"))) {
-                        grid[leftPos] = (grid[leftPos].toLong() + aboveVal).toString()
-                    }
-                    val rightPos = pos + Vector(1, 0)
-                    if (grid[rightPos] == ".") {
-                        grid[rightPos] = aboveVal.toString()
-                    } else if (grid[rightPos].contains(Regex("\\d"))) {
-                        grid[rightPos] = (grid[rightPos].toLong() + aboveVal).toString()
-                    }
-                }
-                if (curr.contains(Regex("\\d"))) {
-                    println(curr)
-                    grid[pos] = (curr.toLong()+aboveVal).toString()
-                }
-            }
-        }
-    }
-    println(grid)
-    println(splits)
-    println(grid.xPositions.map { Vector(it, grid.maxPos.y) }.filter { grid[it].contains(Regex("\\d")) }.map { grid[it].toLong() }.sum())
+    println(grid.xPositions.map { Vector(it, grid.maxPos.y) }.sumOf { grid[it].mul })
 }
