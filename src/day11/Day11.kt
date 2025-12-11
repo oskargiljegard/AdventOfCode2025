997dcecb177d2182e474c343fc456387379f508e
Oskar Giljegard <oskar@giljegard.com>
2025-12-11 06:27:55 +0100

    wip day 11

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -1,12 +1,12 @@
 package day11
 
-import utils.Graph
 import utils.HashmapGraph
-import utils.aStarShortestPath
 import java.io.File
 
+data class PathInfo(val none: Int, val dac: Int, val fft: Int, val dacNfft: Int) {}
+
 fun main() {
-    val lines = File("src/day11/input.txt").readLines()
+    val lines = File("src/day11/input-mini.txt").readLines()
     println(lines)
 
     val graph = HashmapGraph<String>(
@@ -17,16 +17,32 @@ fun main() {
     )
     println(graph)
 
-    val numPaths = graph.graph.keys.associateWith { 0 }.toMutableMap()
+    val numPaths = graph.graph.values.flatten().toSet().associateWith { PathInfo(0, 0, 0, 0) }.toMutableMap()
 
     fun explore(node: String) {
-        numPaths[node] = numPaths.getOrDefault(node, 0) + 1
-        for (n in graph.getNeighbors(node)) {
-            explore(n)
+        val (fromNone, fromDac, fromFft, fromDacNfft) = numPaths[node] ?: error("Node $node not found")
+        for (dest in graph.getNeighbors(node)) {
+            val (destNone, destDac, destFft, destDacNfft) = numPaths[dest] ?: error("Node $dest not found")
+            when (dest) {
+                /*
+                "dac" -> {
+                    numPaths[dest] = PathInfo(destNone, fromNone + fromDac + destDac, destFft, fromFft + fromDacNfft + destDacNfft)
+                }
+                "fft" -> {
+                    numPaths[dest] = PathInfo(destNone, destDac, fromNone + fromFft + destFft, fromDac + fromDacNfft + destDacNfft)
+                }
+                 */
+                else -> {
+                    numPaths[dest] = PathInfo(fromNone + destNone, fromDac + destDac, fromFft + destFft, fromDacNfft + destDacNfft)
+                }
+            }
+            explore(dest)
         }
     }
     println("starting explore")
+    numPaths["you"] = PathInfo(1, 0, 0, 0)
     explore("you")
     println("printing paths")
+    println(numPaths)
     println(numPaths["out"])
 }
