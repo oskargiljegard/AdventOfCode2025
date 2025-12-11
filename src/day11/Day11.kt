0d2dc3f1466494e26fedaac971c6403e38677845
Oskar Giljegard <oskar@giljegard.com>
2025-12-11 06:47:16 +0100

    wip day 11

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -6,7 +6,7 @@ import java.io.File
 data class PathInfo(val none: Int, val dac: Int, val fft: Int, val dacNfft: Int) {}
 
 fun main() {
-    val lines = File("src/day11/input.txt").readLines()
+    val lines = File("src/day11/input-mini2.txt").readLines()
     println(lines)
 
     val graph = HashmapGraph<String>(
@@ -19,20 +19,36 @@ fun main() {
 
     val numPaths = graph.graph.values.flatten().toSet().associateWith { PathInfo(0, 0, 0, 0) }.toMutableMap()
 
-    fun explore(node: String, fft: Boolean=false, dac: Boolean=false) {
+    val visited = mutableSetOf<Pair<String, String>>()
+    fun explore(node: String) {
         val (fromNone, fromDac, fromFft, fromDacNfft) = numPaths[node] ?: error("Node $node not found")
         for (dest in graph.getNeighbors(node)) {
-            val (destNone, destDac, destFft, destDacNfft) = numPaths[dest] ?: error("Node $dest not found")
-            if (fft && dac) {
-                numPaths[dest] = PathInfo(destNone, destDac, destFft, destDacNfft + 1)
-            } else if (fft) {
-                numPaths[dest] = PathInfo(destNone, destDac, destFft + 1, destDacNfft)
-            } else if (dac) {
-                numPaths[dest] = PathInfo(destNone, destDac + 1, destFft, destDacNfft)
-            } else {
-                numPaths[dest] = PathInfo(destNone + 1, destDac, destFft, destDacNfft)
+            if (visited.contains(node to dest))
+                continue
+            visited.add(node to dest)
+            var (destNone, destDac, destFft, destDacNfft) = numPaths[dest] ?: error("Node $dest not found")
+            when (dest) {
+                "fft" -> {
+                    //destNone += fromNone
+                    //destDac += fromDac
+                    destFft += fromFft + fromNone
+                    destDacNfft += fromDacNfft + fromDac
+                }
+                "dac" -> {
+                    //destNone += fromNone
+                    destDac += fromDac + fromNone
+                    //destFft += fromFft
+                    destDacNfft += fromDacNfft + fromFft
+                }
+                else -> {
+                    destNone += fromNone
+                    destDac += fromDac
+                    destFft += fromFft
+                    destDacNfft += fromDacNfft
+                }
             }
-            explore(dest, fft || dest == "fft", dac || dest == "dac")
+            numPaths[dest] = PathInfo(destNone, destDac, destFft, destDacNfft)
+            explore(dest)
         }
     }
     println("starting explore")
